27fcb9dce9222adaceef47f85211e9cab1444cdb
Muhammad Taus <md.taus03@gmail.com>
2024-10-21 15:21:01 +0600

    share section mobile

---
+++ index.js
@@ -0,0 +1,9 @@
+const shareBtn = document.getElementById("icon");
+const displayShare =  document.getElementById("display-share");
+
+shareBtn.addEventListener("click", () => {
+    console.log(shareBtn);
+    console.log(displayShare);
+}); 
+
+alert('Worked!');
